c2cbc02940a6b83703ad618ff977e7d3c7d82c68
kyle o'neill <kyleoneill24@gmail.com>
2022-07-04 02:17:11 -0400

    Add unit tests for vector push_front, pop_front, insert, remove

--- data_structures/cvec/doctest_tests/CVecTests.cpp
+++ data_structures/cvec/doctest_tests/CVecTests.cpp
@@ -64,3 +64,65 @@ TEST_CASE("Vector pop_back") {
     // Verify that after popping back, the pop-backed indexes are no longer accessible
     CHECK_THROWS_AS(some_vec[15], std::runtime_error);
 }
+
+TEST_CASE("Vector push_front") {
+    Vec<int> some_vec{};
+    for(int i = 0; i < 20; i++) {
+        some_vec.push_back(i);
+    }
+
+    // push_front and verify that the new length is correct and the new values were added to the front
+    for(int i = 0; i < 5; i++) {
+        some_vec.push_front(100);
+    }
+    CHECK(some_vec.len() == 25);
+    CHECK(some_vec[0] == 100);
+    CHECK(some_vec[some_vec.len() - 1] == 19);
+}
+
+TEST_CASE("Vector pop_front") {
+    Vec<int> some_vec{};
+    for(int i = 0; i < 20; i++) {
+        some_vec.push_back(i);
+    }
+
+    // pop_front and verify that the new length is correct and values were removed from the front
+    for(int i = 0; i < 5; i++) {
+        some_vec.pop_front();
+    }
+    CHECK(some_vec.len() == 15);
+    CHECK(some_vec[0] == 5);
+    CHECK(some_vec[some_vec.len() - 1] == 19);
+}
+
+TEST_CASE("Vector insert") {
+    Vec<int> some_vec{};
+    for(int i = 0; i < 20; i++) {
+        some_vec.push_back(i);
+    }
+
+    // Insert at some index, make sure that the first/last and values around that index are all correct
+    some_vec.insert(100, 7);
+    CHECK(some_vec.len() == 21);
+    CHECK(some_vec[0] == 0);
+    CHECK(some_vec[some_vec.len() - 1] == 19);
+    CHECK(some_vec[6] == 6);
+    CHECK(some_vec[7] == 100);
+    CHECK(some_vec[8] == 7);
+}
+
+TEST_CASE("Vector remove") {
+    Vec<int> some_vec{};
+    for(int i = 0; i < 20; i++) {
+        some_vec.push_back(i);
+    }
+
+    // Remove from some index, make sure that the first/last and values around that index are all correct
+    some_vec.remove(10);
+    CHECK(some_vec.len() == 19);
+    CHECK(some_vec[0] == 0);
+    CHECK(some_vec[some_vec.len() - 1] == 19);
+    CHECK(some_vec[9] == 9);
+    CHECK(some_vec[10] == 11);
+    CHECK(some_vec[11] == 12);
+}
